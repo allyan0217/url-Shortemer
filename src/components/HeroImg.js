ae0d0a7d882b97fb8fc63ecdf812845f0f5e0faf
Inoxentkhan <m.nisarshinwar29@gmail.com>
2023-05-06 00:55:56 +0500

    nisar's commit

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,56 +1,81 @@
 import "./HeroImgStyles.css";
-import React, { useState } from "react";
 import IntroImg from "../assets/bg_01.jpg";
 import { BsArrowRepeat } from "react-icons/bs";
 import "../index.css";
-const HeroImg = () => {
-  const [link, setLink] = useState("");
-  const [newLink, setNewLink] = useState("");
-  const [expiryDate, setExpiryDate] = useState("");
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
+//Actual logic
+const ACCESS_TOKEN = "f3bafbec8fa2149fdc2982844145cf8d1f4dd269";
 
-  const handleLinkChange = (e) => {
-    setLink(e.target.value);
-    setNewLink(link);
+function App() {
+  //declaration
+  const [url, setUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+  const [expirationDate, setExpirationDate] = useState(null);
+  const [urlList, setUrlList] = useState([]);
+
+  //for local storage
+  useEffect(() => {
+    const savedUrls = JSON.parse(localStorage.getItem("urlList") || "[]");
+    setUrlList(savedUrls);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("urlList", JSON.stringify(urlList));
+  }, [urlList]);
+
+  //handle submit Api Call
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch(`https://api-ssl.bitly.com/v4/shorten`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify({
+        long_url: url,
+        expires_at: expirationDate && expirationDate.toISOString(),
+      }),
+    });
+    const data = await response.json();
+    setShortUrl(data.link);
+    setUrlList([...urlList, { longUrl: url, shortUrl: data.link }]);
+
+    const handleDate = (date) => {
+      setExpirationDate(date.target.value);
+    };
   };
 
-  const handleDate = (e) => { 
-    setExpiryDate(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    console.log({ link, newLink, expiryDate });
-
-    const arrayLinks = JSON.parse(localStorage.getItem("arrayLinks")) || [];
-    localStorage.setItem(
-      "arrayLinks",
-      JSON.stringify([...arrayLinks, { link, newLink, expiryDate }])
-    );
-  };
   return (
     <div className="hero">
       <div className="mask">
         <img className="into-img" src={IntroImg} alt="intro-img" />
       </div>
+
       <div className="content">
         {/* <p>Hi! I am a Software Developer</p> */}
         <h1>URL-Shortener</h1>
-        <div className="border"> 
-          <form className="link">
+        <div className="border">
+          <form onSubmit={handleSubmit} className="link">
             <input
               type="date"
-              value={expiryDate}
-              onChange={(e) => handleDate(e)}
+              selected={expirationDate}
+              onChange={(date) => setExpirationDate(date)}
+              minDate={new Date()}
               className="set-date"
               placeholder="Expiry date"
             />
             <input
               type="text"
-              value={link}
-              onChange={handleLinkChange}
+              value={url}
               className="link-input"
-              placeholder="Paste link or URL"
+              placeholder="Paste link or url"
               size="50"
+              onChange={(event) => setUrl(event.target.value)}
             />
+
             <button type="button" onClick={handleSubmit} className="submit">
               <BsArrowRepeat
                 size={20}
@@ -58,10 +83,41 @@ const HeroImg = () => {
               />
             </button>
           </form>
+          {shortUrl && (
+            <div>
+              <p>Shortened URL:</p>
+              <a href={shortUrl} target="_blank" rel="noopener noreferrer">
+                {shortUrl}
+              </a>
+              {expirationDate && (
+                <p>Expires on: {expirationDate.toLocaleDateString()}</p>
+              )}
+            </div>
+          )}
+          {urlList.length > 0 && (
+            <div>
+              <h2>Recent URLs</h2>
+              <ul>
+                {urlList.map((urlItem, index) => (
+                  <li key={index}>
+                    <a
+                      href={urlItem.shortUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {urlItem.shortUrl}
+                    </a>
+                    <br />
+                    <small>{urlItem.longUrl}</small>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default HeroImg;
+export default App;
--- src/routes/History.js
+++ src/routes/History.js
@@ -1,43 +1,134 @@
+import "./history.css";
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
 import HeroImg2 from "../components/HeroImg2";
 import { MdDelete } from "react-icons/md";
+import { useState, useEffect } from "react";
+import "./history.css";
 
-const History = () => {
-  const heading = "HISTORY";
-  const [arrayLinks, setArrayLinks] = useState([]);
+function History() {
+  const [urlList, setUrlList] = useState([]);
 
   useEffect(() => {
-    const arrayLinks = localStorage.getItem("arrayLinks");
-    setArrayLinks(JSON.parse(arrayLinks));
+    const storedUrls = JSON.parse(localStorage.getItem("urlList"));
+    if (storedUrls) {
+      setUrlList(storedUrls);
+    }
   }, []);
+  const handleDeleteUrl = (id) => {
+    const newUrlList = urlList.filter((urlItem) => urlItem.id !== id);
+    setUrlList(newUrlList);
+    localStorage.setItem("urlList", JSON.stringify(newUrlList));
+  };
 
-  const handleDelete = (arrayIndex) => {};
+  const handleCopyShortUrl = (shortUrl) => {
+    navigator.clipboard.writeText(shortUrl);
+  };
+
+  const handleEditExpirationDate = (urlItem) => {
+    const newExpirationDate = prompt("Enter new expiration date (YYYY-MM-DD):");
+    const parsedNewExpirationDate =
+      newExpirationDate && new Date(newExpirationDate);
+    const newUrlList = urlList.map((item) => {
+      if (item.id === urlItem.id) {
+        return {
+          ...item,
+          expirationDate: parsedNewExpirationDate,
+        };
+      } else {
+        return item;
+      }
+    });
+    setUrlList(newUrlList);
+    localStorage.setItem("urlList", JSON.stringify(newUrlList));
+  };
 
   return (
-    <>
-      <div>
-        <HeroImg2 heading={heading} />
-        {arrayLinks && arrayLinks.length
-          ? arrayLinks.map((arrayLinks, arrayIndex) => {
-              return (
-                <div>
-                  Link - {arrayLinks.link}
-                  <br />
-                  NewLink - {arrayLinks.newLink}
-                  <br />
-                  ExpiryDate - {arrayLinks.expiryDate}
-                  <MdDelete onClick={() => handleDelete(arrayIndex)}></MdDelete>
-                </div>
-              );
-            })
-          : "array is empty"}
-
-        {/* <HeroImg/> */}
-      </div>
-    </>
+    <div>
+      <h1>History</h1>
+      {urlList.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Long URL</th>
+              <th>Short URL</th>
+              <th>Expiration Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {urlList.map((urlItem) => (
+              <tr key={urlItem.id}>
+                <td>{urlItem.longUrl}</td>
+                <td>
+                  <a
+                    href={urlItem.shortUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {urlItem.shortUrl}
+                  </a>
+                </td>
+                <td>
+                  {urlItem.expirationDate?.toLocaleDateString() ?? "Never"}
+                </td>
+                <td>
+                  <button onClick={() => handleCopyShortUrl(urlItem.shortUrl)}>
+                    Copy
+                  </button>
+                  <button onClick={() => handleEditExpirationDate(urlItem)}>
+                    Edit Expiration
+                  </button>
+                  <button onClick={() => handleDeleteUrl(urlItem.id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No previously shortened URLs found.</p>
+      )}
+    </div>
   );
-};
+}
 
 export default History;
+
+// const History = () => {
+//   const heading = "HISTORY";
+//   const [arrayLinks, setArrayLinks] = useState([]);
+
+//   useEffect(() => {
+//     const arrayLinks = localStorage.getItem("arrayLinks");
+//     setArrayLinks(JSON.parse(arrayLinks));
+//   }, []);
+
+//   const handleDelete = (arrayIndex) => {};
+
+//   return (
+//     <>
+//       <div>
+//         <HeroImg2 heading={heading} />
+//         {arrayLinks && arrayLinks.length
+//           ? arrayLinks.map((arrayLinks, arrayIndex) => {
+//               return (
+//                 <div>
+//                   Link - {arrayLinks.link}
+//                   <br />
+//                   NewLink - {arrayLinks.newLink}
+//                   <br />
+//                   ExpiryDate - {arrayLinks.expiryDate}
+//                   <MdDelete onClick={() => handleDelete(arrayIndex)}></MdDelete>
+//                 </div>
+//               );
+//             })
+//           : "array is empty"}
+
+//         {/* <HeroImg/> */}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default History;
