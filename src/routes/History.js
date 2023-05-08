37a1abcbcc298b2b657b0fbf0dc52cf8a5b1de98
allyan0217 <allyan0217@gmail.com>
2023-05-08 15:18:30 +0500

    allyan's commit

--- src/routes/History.js
+++ src/routes/History.js
@@ -1,96 +1,102 @@
-import "./history.css";
-import React from "react";
-import HeroImg2 from "../components/HeroImg2";
-import { MdDelete } from "react-icons/md";
-import { useState, useEffect } from "react";
-import "./history.css";
+import './history.css';
+import React from 'react';
+import HeroImg2 from '../components/HeroImg2';
+import { MdDelete } from 'react-icons/md';
+import { useState, useEffect } from 'react';
+import './history.css';
 
 function History() {
-  const [urlList, setUrlList] = useState([]);
+	const [urlList, setUrlList] = useState([]);
 
-  useEffect(() => {
-    const storedUrls = JSON.parse(localStorage.getItem("urlList"));
-    if (storedUrls) {
-      setUrlList(storedUrls);
+	useEffect(() => {
+		const storedUrls = JSON.parse(localStorage.getItem('urlList'));
+		if (storedUrls) {
+			setUrlList(storedUrls);
+		}
+	}, []);
+
+  const handleDeleteUrl = id => {
+    const urlIndex = urlList.findIndex(urlItem => urlItem.id === id);
+    if (urlIndex === -1) {
+      return; // the item was not found in the array, so there's nothing to delete
     }
-  }, []);
-  const handleDeleteUrl = (id) => {
-    const newUrlList = urlList.filter((urlItem) => urlItem.id !== id);
+    const newUrlList = [...urlList];
+    newUrlList.splice(urlIndex, 1);
+    localStorage.setItem('urlList', JSON.stringify(newUrlList));
     setUrlList(newUrlList);
-    localStorage.setItem("urlList", JSON.stringify(newUrlList));
   };
 
-  const handleCopyShortUrl = (shortUrl) => {
-    navigator.clipboard.writeText(shortUrl);
-  };
+	const handleCopyShortUrl = shortUrl => {
+		navigator.clipboard.writeText(shortUrl);
+	};
 
-  const handleEditExpirationDate = (urlItem) => {
-    const newExpirationDate = prompt("Enter new expiration date (YYYY-MM-DD):");
-    const parsedNewExpirationDate =
-      newExpirationDate && new Date(newExpirationDate);
-    const newUrlList = urlList.map((item) => {
-      if (item.id === urlItem.id) {
-        return {
-          ...item,
-          expirationDate: parsedNewExpirationDate,
-        };
-      } else {
-        return item;
-      }
-    });
-    setUrlList(newUrlList);
-    localStorage.setItem("urlList", JSON.stringify(newUrlList));
-  };
+	const handleEditExpirationDate = urlItem => {
+		const newExpirationDate = prompt('Enter new expiration date (YYYY-MM-DD):');
+		const parsedNewExpirationDate =
+			newExpirationDate && new Date(newExpirationDate);
+		const newUrlList = urlList.map(item => {
+			if (item.id === urlItem.id) {
+				return {
+					...item,
+					expirationDate: parsedNewExpirationDate,
+				};
+			} else {
+				return item;
+			}
+		});
+		setUrlList(newUrlList);
+		localStorage.setItem('urlList', JSON.stringify(newUrlList));
+	};
 
-  return (
-    <div>
-      <h1>History</h1>
-      {urlList.length > 0 ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Long URL</th>
-              <th>Short URL</th>
-              <th>Expiration Date</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {urlList.map((urlItem) => (
-              <tr key={urlItem.id}>
-                <td>{urlItem.longUrl}</td>
-                <td>
-                  <a
-                    href={urlItem.shortUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {urlItem.shortUrl}
-                  </a>
-                </td>
-                <td>
-                  {urlItem.expirationDate?.toLocaleDateString() ?? "Never"}
-                </td>
-                <td>
-                  <button onClick={() => handleCopyShortUrl(urlItem.shortUrl)}>
-                    Copy
-                  </button>
-                  <button onClick={() => handleEditExpirationDate(urlItem)}>
-                    Edit Expiration
-                  </button>
-                  <button onClick={() => handleDeleteUrl(urlItem.id)}>
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p>No previously shortened URLs found.</p>
-      )}
-    </div>
-  );
+	return (
+		<div className='history'>
+			<h1>History</h1>
+			{urlList.length > 0 ? (
+				<table>
+					<thead>
+						<tr>
+							<th>Long URL</th>
+							<th>Short URL</th>
+							<th>Expiration Date</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{urlList.map(urlItem => (
+							<tr key={urlItem.id}>
+								<td>{urlItem.longUrl}</td>
+								<td>
+									<a
+										href={urlItem.shortUrl}
+										target='_blank'
+										rel='noopener noreferrer'
+									>
+										{urlItem.shortUrl}
+									</a>
+								</td>
+								<td>
+									{urlItem.expirationDate?.toLocaleDateString() ?? 'Never'}
+								</td>
+								<td>
+									<button onClick={() => handleCopyShortUrl(urlItem.shortUrl)}>
+										Copy
+									</button>
+									<button onClick={() => handleEditExpirationDate(urlItem)}>
+										Edit Expiration
+									</button>
+									<button onClick={() => handleDeleteUrl(urlItem.id)}>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			) : (
+				<p>No previously shortened URLs found.</p>
+			)}
+		</div>
+	);
 }
 
 export default History;
